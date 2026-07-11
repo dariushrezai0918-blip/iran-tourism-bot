f1e297edf0795dc476d75f62e141cb248838c451
dariushrezai0918-blip <dariushrezai0918@gmail.com>
2026-07-11 13:10:30 +0330

    app.py

--- app.py
+++ app.py
@@ -1,24 +1,46 @@
+import time
+import schedule
+
 from world_places import WORLD_PLACES
 from wikipedia_source import get_summary
 from formatter import format_post
 
-place = WORLD_PLACES[0]
 
-data = get_summary(place["title"])
+def publish_post():
 
-if data:
+    place = WORLD_PLACES[0]
 
-    post = format_post(
-        data["title"],
-        place["country"],
-        data["description"]
-    )
+    data = get_summary(place["title"])
 
-    print(post)
+    if data:
 
-    print("\nتصویر:\n")
-    print(data["image"])
+        post = format_post(
+            data["title"],
+            place["country"],
+            data["description"]
+        )
 
-else:
-    print("اطلاعات دریافت نشد.")
-  
+        print("=" * 40)
+        print(post)
+
+        if data["image"]:
+            print("\n📷 تصویر:")
+            print(data["image"])
+
+        print("=" * 40)
+
+    else:
+        print("دریافت اطلاعات ناموفق بود.")
+
+
+# اجرای آزمایشی هنگام شروع
+publish_post()
+
+# زمان‌بندی (فعلاً هر 1 دقیقه برای تست)
+schedule.every(1).minutes.do(publish_post)
+
+print("🤖 ربات آماده است...")
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
