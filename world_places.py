991959a5d0da670aca5827e20a276f56a4c78dc5
dariushrezai0918-blip <dariushrezai0918@gmail.com>
2026-07-11 12:39:34 +0330

    Create world_places.py

---
+++ world_places.py
@@ -0,0 +1,27 @@
+WORLD_PLACES = [
+    {
+        "title": "برج ایفل",
+        "country": "فرانسه",
+        "wiki": "https://fa.wikipedia.org/wiki/برج_ایفل"
+    },
+    {
+        "title": "دیوار بزرگ چین",
+        "country": "چین",
+        "wiki": "https://fa.wikipedia.org/wiki/دیوار_بزرگ_چین"
+    },
+    {
+        "title": "ماچو پیچو",
+        "country": "پرو",
+        "wiki": "https://fa.wikipedia.org/wiki/ماچو_پیچو"
+    },
+    {
+        "title": "تاج محل",
+        "country": "هند",
+        "wiki": "https://fa.wikipedia.org/wiki/تاج_محل"
+    },
+    {
+        "title": "آبشار نیاگارا",
+        "country": "کانادا",
+        "wiki": "https://fa.wikipedia.org/wiki/آبشار_نیاگارا"
+    }
+]
