628914664e996d32b8dd1de9d127332718fe2e6e
dariushrezai0918-blip <dariushrezai0918@gmail.com>
2026-07-11 21:15:28 +0330

    scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,12 +1,28 @@
 import time
 import schedule
 
+from world_places import WORLD_PLACES
+from wikipedia_source import get_summary
+
 def job():
-    print("ربات در حال اجراست...")
+    place = WORLD_PLACES[0]   # فعلاً اولین مکان را تست می‌کنیم
+
+    print(f"در حال دریافت اطلاعات: {place['title']}")
+
+    data = get_summary(place["title"])
+
+    if data:
+        print("✅ اطلاعات دریافت شد")
+        print(data["title"])
+        print(data["description"][:200])
+    else:
+        print("❌ دریافت اطلاعات ناموفق بود")
 
 schedule.every(1).minutes.do(job)
 
-print("Scheduler Started")
+print("🤖 ربات آماده است...")
+
+job()
 
 while True:
     schedule.run_pending()
