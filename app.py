a2c0953ee5c1eb852403172dcd8844a7eb0abe62
dariushrezai0918-blip <dariushrezai0918@gmail.com>
2026-07-11 21:57:37 +0330

    app.py

--- app.py
+++ app.py
@@ -1,46 +1,11 @@
-import time
-import schedule
+from generator import generate_post
+from publisher import publish
 
-from world_places import WORLD_PLACES
-from wikipedia_source import get_summary
-from formatter import format_post
+print("ربات گردشگری ایران شروع شد...")
 
+post = generate_post()
 
-def publish_post():
-
-    place = WORLD_PLACES[0]
-
-    data = get_summary(place["title"])
-
-    if data:
-
-        post = format_post(
-            data["title"],
-            place["country"],
-            data["description"]
-        )
-
-        print("=" * 40)
-        print(post)
-
-        if data["image"]:
-            print("\n📷 تصویر:")
-            print(data["image"])
-
-        print("=" * 40)
-
-    else:
-        print("دریافت اطلاعات ناموفق بود.")
-
-
-# اجرای آزمایشی هنگام شروع
-publish_post()
-
-# زمان‌بندی (فعلاً هر 1 دقیقه برای تست)
-schedule.every(1).minutes.do(publish_post)
-
-print("🤖 ربات آماده است...")
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+if post:
+    publish(post)
+else:
+    print("خطا در تولید محتوا")
