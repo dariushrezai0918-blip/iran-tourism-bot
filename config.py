a00e640f2948a1bd488a87889e05ec49f7bc8f73
dariushrezai0918-blip <dariushrezai0918@gmail.com>
2026-07-11 12:31:08 +0330

    Create config.py
    # تنظیمات ربات گردشگری جهان
    
    BOT_NAME = "World Tourism Bot"
    
    POSTS_PER_DAY = 3
    
    POST_HOURS = [
        "09:00",
        "15:00",
        "21:00"
    ]
    
    LANGUAGE = "fa"
    
    IMAGE_SOURCE = "wikimedia"
    
    TEXT_SOURCE = "wikipedia"
    
    USE_AI = False
    
    HASHTAGS = [
        "#گردشگری",
        "#جهانگردی",
        "#جاذبه_گردشگری",
        "#سفر",
        "#مکان_دیدنی"
    ]

---
+++ config.py
@@ -0,0 +1,27 @@
+# تنظیمات ربات گردشگری جهان
+
+BOT_NAME = "World Tourism Bot"
+
+POSTS_PER_DAY = 3
+
+POST_HOURS = [
+    "09:00",
+    "15:00",
+    "21:00"
+]
+
+LANGUAGE = "fa"
+
+IMAGE_SOURCE = "wikimedia"
+
+TEXT_SOURCE = "wikipedia"
+
+USE_AI = False
+
+HASHTAGS = [
+    "#گردشگری",
+    "#جهانگردی",
+    "#جاذبه_گردشگری",
+    "#سفر",
+    "#مکان_دیدنی"
+]
