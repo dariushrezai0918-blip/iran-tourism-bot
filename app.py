f84585936a9166037a584794dcd6f7dc00063896
dariushrezai0918-blip <dariushrezai0918@gmail.com>
2026-07-11 12:47:49 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
-print("Iran Tourism Bot Started")
+from world_places import WORLD_PLACES
+from wikipedia_source import get_summary
+from formatter import format_post
+
+place = WORLD_PLACES[0]
+
+data = get_summary(place["title"])
+
+if data:
+
+    post = format_post(
+        data["title"],
+        place["country"],
+        data["description"]
+    )
+
+    print(post)
+
+    print("\nتصویر:\n")
+    print(data["image"])
+
+else:
+    print("اطلاعات دریافت نشد.")
