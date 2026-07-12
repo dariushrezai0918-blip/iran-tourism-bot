f80f4fca366fcbce1b1f0c1245df00ad84575eae
dariushrezai0918-blip <dariushrezai0918@gmail.com>
2026-07-12 09:57:34 +0330

    app.py

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 from generator import generate_post
 from publisher import publish
+import schedule
+import time
 
-print("ربات گردشگری ایران شروع شد...")
+print("🌍 ربات گردشگری جهان شروع شد...")
 
-post = generate_post()
+def job():
+    print("در حال تولید پست جدید...")
 
-if post:
-    publish(post)
-else:
-    print("خطا در تولید محتوا")
+    post = generate_post()
+
+    if post:
+        publish(post)
+    else:
+        print("❌ خطا در تولید محتوا")
+
+# یک بار هنگام شروع اجرا شود
+job()
+
+# هر ۶ ساعت یک بار اجرا شود
+schedule.every(6).hours.do(job)
+
+# همیشه روشن بماند
+while True:
+    schedule.run_pending()
+    time.sleep(30)
