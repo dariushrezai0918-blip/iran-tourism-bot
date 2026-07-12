8b1e3d2a28236f9d8d01e8a713d5fb4d65538102
dariushrezai0918-blip <dariushrezai0918@gmail.com>
2026-07-12 18:51:55 +0330

    telegram_sender.py

--- telegram_sender.py
+++ telegram_sender.py
@@ -1,16 +1,18 @@
 import os
-import asyncio
-from telegram import Bot
+import requests
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME")
 
-async def send_message(text):
-    bot = Bot(token=BOT_TOKEN)
-    await bot.send_message(
-        chat_id=CHANNEL_USERNAME,
-        text=text
-    )
+def publish(text):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-def publish(post):
-    asyncio.run(send_message(post))
+    data = {
+        "chat_id": CHANNEL_USERNAME,
+        "text": text
+    }
+
+    response = requests.post(url, data=data)
+
+    print("Telegram:", response.status_code)
+    print(response.text)
