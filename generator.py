7df4fd1c14242ec7d1091cf250cf3a0f8f825958
dariushrezai0918-blip <dariushrezai0918@gmail.com>
2026-07-12 09:47:47 +0330

    generator.py

--- generator.py
+++ generator.py
@@ -1,23 +1,15 @@
 import random
-
 from world_places import WORLD_PLACES
-from wikipedia_source import get_summary
 from formatter import format_post
-
 
 def generate_post():
     place = random.choice(WORLD_PLACES)
 
-    data = get_summary(place["title"])
-
-    if not data:
-        return None
-
     info = {
         "title": place["title"],
         "country": place["country"],
-        "description": data["description"],
-        "image": data["image"],
+        "description": place["description"],
+        "image": place["image"],
         "wiki": place["wiki"]
     }
 
