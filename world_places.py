354f666ada947b75af9b523675b0fee17d7d13d1
dariushrezai0918-blip <dariushrezai0918@gmail.com>
2026-07-12 09:43:58 +0330

    world_places.py

--- world_places.py
+++ world_places.py
@@ -1,27 +1,27 @@
 WORLD_PLACES = [
+
     {
         "title": "برج ایفل",
         "country": "فرانسه",
+        "description": "برج ایفل مشهورترین نماد شهر پاریس است و سالانه میلیون‌ها گردشگر از آن بازدید می‌کنند.",
+        "image": "",
         "wiki": "https://fa.wikipedia.org/wiki/برج_ایفل"
     },
+
     {
         "title": "دیوار بزرگ چین",
         "country": "چین",
+        "description": "دیوار بزرگ چین یکی از بزرگ‌ترین سازه‌های ساخته‌شده توسط انسان و از عجایب جهان است.",
+        "image": "",
         "wiki": "https://fa.wikipedia.org/wiki/دیوار_بزرگ_چین"
     },
+
     {
-        "title": "ماچو پیچو",
-        "country": "پرو",
-        "wiki": "https://fa.wikipedia.org/wiki/ماچو_پیچو"
-    },
-    {
-        "title": "تاج محل",
-        "country": "هند",
-        "wiki": "https://fa.wikipedia.org/wiki/تاج_محل"
-    },
-    {
-        "title": "آبشار نیاگارا",
-        "country": "کانادا",
-        "wiki": "https://fa.wikipedia.org/wiki/آبشار_نیاگارا"
+        "title": "کولوسئوم",
+        "country": "ایتالیا",
+        "description": "کولوسئوم رم یکی از معروف‌ترین آمفی‌تئاترهای باستانی جهان است.",
+        "image": "",
+        "wiki": "https://fa.wikipedia.org/wiki/کولوسئوم"
     }
+
 ]
