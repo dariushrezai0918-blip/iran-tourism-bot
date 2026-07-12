16477826895c3663b5f4f7cbb1651c07c243482f
dariushrezai0918-blip <dariushrezai0918@gmail.com>
2026-07-12 09:34:03 +0330

    wikipedia_source.py

--- wikipedia_source.py
+++ wikipedia_source.py
@@ -1,16 +1,13 @@
 import requests
 
 def get_summary(title):
-    """
-    دریافت خلاصه مقاله از ویکی‌پدیای فارسی
-    """
-
     url = f"https://fa.wikipedia.org/api/rest_v1/page/summary/{title}"
 
     try:
         response = requests.get(url, timeout=15)
 
         if response.status_code != 200:
+            print("Wikipedia Error:", response.status_code)
             return None
 
         data = response.json()
@@ -18,9 +15,9 @@ def get_summary(title):
         return {
             "title": data.get("title", ""),
             "description": data.get("extract", ""),
-            "image": data.get("thumbnail", {}).get("source")
+            "image": data.get("thumbnail", {}).get("source", "")
         }
 
     except Exception as e:
-        print(e)
+        print("Wikipedia Exception:", e)
         return None
