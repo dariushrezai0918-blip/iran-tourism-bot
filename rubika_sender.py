d825a632f17e0bc5ec46300d625960c446ce5a09
dariushrezai0918-blip <dariushrezai0918@gmail.com>
2026-07-13 08:54:29 +0330

    rubika_sender.py

--- rubika_sender.py
+++ rubika_sender.py
@@ -1,1 +1,20 @@
+import os
+import requests
 
+TOKEN = os.getenv("RUBIKA_TOKEN")
+CHAT_ID = os.getenv("RUBIKA_CHAT_ID")
+
+
+def publish(post):
+
+    url = f"https://botapi.rubika.ir/v3/{TOKEN}/sendMessage"
+
+    data = {
+        "chat_id": CHAT_ID,
+        "text": post
+    }
+
+    response = requests.post(url, json=data)
+
+    print("Rubika response:")
+    print(response.text)
