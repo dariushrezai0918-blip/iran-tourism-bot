0a58c26e896b5d325825052e6c259a5c45b6a219
dariushrezai0918-blip <dariushrezai0918@gmail.com>
2026-07-13 19:06:39 +0330

    generator.py

--- generator.py
+++ generator.py
@@ -6,16 +6,21 @@ def generate_post():
 
     place = get_random_place()
 
+    print("Wikipedia result:", place)
+
     if not place:
+        print("❌ هیچ مکانی از ویکی‌پدیا دریافت نشد")
         return None
 
     info = {
         "title": place["title"],
         "description": place["description"],
-        "image": place["image"],
-        "wiki": place["wiki"]
+        "image": place.get("image", ""),
+        "wiki": place.get("wiki", "")
     }
 
     post = format_post(info)
+
+    print("Generated post:", post)
 
     return post
