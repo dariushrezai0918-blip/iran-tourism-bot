169918a0c0f48398011bd56ce20d7feddcf585bc
dariushrezai0918-blip <dariushrezai0918@gmail.com>
2026-07-13 14:27:28 +0330

    wikipedia_source.py

--- wikipedia_source.py
+++ wikipedia_source.py
@@ -1,4 +1,16 @@
 import requests
+import random
+
+
+KEYWORDS = [
+    "جاذبه گردشگری",
+    "میراث جهانی یونسکو",
+    "بنای تاریخی",
+    "مکان دیدنی",
+    "پارک ملی",
+    "موزه"
+]
+
 
 def get_summary(title):
     url = f"https://fa.wikipedia.org/api/rest_v1/page/summary/{title}"
@@ -15,9 +27,40 @@ def get_summary(title):
         return {
             "title": data.get("title", ""),
             "description": data.get("extract", ""),
-            "image": data.get("thumbnail", {}).get("source", "")
+            "image": data.get("thumbnail", {}).get("source", ""),
+            "wiki": data.get("content_urls", {}).get("desktop", {}).get("page", "")
         }
 
     except Exception as e:
         print("Wikipedia Exception:", e)
+        return None
+
+
+
+def get_random_place():
+
+    keyword = random.choice(KEYWORDS)
+
+    url = "https://fa.wikipedia.org/w/api.php"
+
+    params = {
+        "action": "query",
+        "list": "search",
+        "srsearch": keyword,
+        "format": "json",
+        "srlimit": 20
+    }
+
+    try:
+        response = requests.get(url, params=params, timeout=15)
+        data = response.json()
+
+        results = data["query"]["search"]
+
+        title = random.choice(results)["title"]
+
+        return get_summary(title)
+
+    except Exception as e:
+        print("Random place error:", e)
         return None
