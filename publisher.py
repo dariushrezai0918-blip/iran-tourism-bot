dafd919bd813fa678964e1655a049f560bdd259b
dariushrezai0918-blip <dariushrezai0918@gmail.com>
2026-07-13 12:50:45 +0330

    publisher.py

--- publisher.py
+++ publisher.py
@@ -1,1 +1,18 @@
-from telegram_sender import publish
+from telegram_sender import publish as telegram_publish
+from rubika_sender import publish as rubika_publish
+
+
+def publish(post):
+    # ارسال به تلگرام
+    try:
+        telegram_publish(post)
+        print("✅ ارسال تلگرام موفق بود")
+    except Exception as e:
+        print("❌ خطای تلگرام:", e)
+
+    # ارسال به روبیکا
+    try:
+        rubika_publish(post)
+        print("✅ ارسال روبیکا موفق بود")
+    except Exception as e:
+        print("❌ خطای روبیکا:", e)
