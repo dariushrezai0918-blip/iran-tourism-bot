5769ef5970d5eec4af58a1a09cc224f8ebd3d414
dariushrezai0918-blip <dariushrezai0918@gmail.com>
2026-07-14 09:37:01 +0330

    app.py

--- app.py
+++ app.py
@@ -8,28 +8,46 @@ import os
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return "Bot is running!"
 
+
 def job():
-    print("در حال تولید پست جدید...")
+    print("🚀 شروع تولید پست...")
 
-    post = generate_post()
+    try:
+        post = generate_post()
 
-    if post:
-        publish(post)
-    else:
-        print("خطا در تولید محتوا")
+        print("📄 نتیجه generate_post:", post)
+
+        if post:
+            publish(post)
+            print("✅ پست با موفقیت ارسال شد")
+        else:
+            print("❌ پست تولید نشد")
+
+    except Exception as e:
+        print("🔥 خطا در job:", e)
+
 
 def scheduler():
+    print("⏰ Scheduler Started")
+
+    # اجرای اولیه
     job()
-   schedule.every(1).minutes.do(job)
+
+    # برای تست هر یک دقیقه
+    schedule.every(1).minutes.do(job)
+
     while True:
         schedule.run_pending()
-        time.sleep(30)
+        time.sleep(5)
+
 
 Thread(target=scheduler, daemon=True).start()
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
