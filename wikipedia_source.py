1fa63da68ba355bc189c5467e7486bf8cb521966
dariushrezai0918-blip <dariushrezai0918@gmail.com>
2026-07-14 12:26:43 +0330

    wikipedia_source.py

--- wikipedia_source.py
+++ wikipedia_source.py
@@ -1,47 +1,97 @@
 import requests
+import random
+
+
+KEYWORDS = [
+    "جاذبه گردشگری",
+    "میراث جهانی یونسکو",
+    "بنای تاریخی",
+    "مکان دیدنی جهان"
+]
 
 
 def get_random_place():
 
-    url = "https://fa.wikipedia.org/api/rest_v1/page/random/summary"
+    keyword=random.choice(KEYWORDS)
+
+    url="https://fa.wikipedia.org/w/api.php"
+
+
+    params={
+        "action":"query",
+        "list":"search",
+        "srsearch":keyword,
+        "format":"json",
+        "srlimit":10
+    }
+
 
     try:
-        print("🎲 دریافت صفحه تصادفی ویکی‌پدیا")
 
-        response = requests.get(
+        response=requests.get(
             url,
+            params=params,
             headers={
-                "User-Agent": "TourismBot/1.0"
+                "User-Agent":"TourismBot"
             },
             timeout=10
         )
 
-        print("📡 وضعیت ویکی‌پدیا:", response.status_code)
 
-        if response.status_code != 200:
-            print("❌ خطا در دریافت اطلاعات")
-            return None
+        data=response.json()
 
-        data = response.json()
+        results=data["query"]["search"]
 
-        title = data.get("title", "")
-        description = data.get("extract", "")
+        title=random.choice(results)["title"]
 
-        if not title or not description:
-            print("❌ اطلاعات ناقص")
-            return None
 
-        print("✅ انتخاب شد:", title)
+        return get_summary(title)
 
-        return {
-            "title": title,
-            "description": description,
-            "image": data.get("thumbnail", {}).get("source", ""),
-            "wiki": data.get("content_urls", {})
-                         .get("desktop", {})
-                         .get("page", "")
-        }
 
     except Exception as e:
-        print("❌ خطای ویکی‌پدیا:", e)
+        print("Wiki error:",e)
+        return None
+
+
+
+
+def get_summary(title):
+
+    url=f"https://fa.wikipedia.org/api/rest_v1/page/summary/{title}"
+
+
+    try:
+
+        r=requests.get(
+            url,
+            headers={
+                "User-Agent":"TourismBot"
+            },
+            timeout=10
+        )
+
+
+        data=r.json()
+
+
+        return {
+
+            "title":data.get("title"),
+
+            "description":
+            data.get("extract",""),
+
+            "image":
+            data.get("thumbnail",{}).get("source",""),
+
+            "wiki":
+            data.get("content_urls",{})
+            .get("desktop",{})
+            .get("page","")
+
+        }
+
+
+    except Exception as e:
+        print(e)
         return None
